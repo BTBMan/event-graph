6a62b484d6d3989d60d956b463611fbbfe1381d3
BTBMan <1498118157@qq.com>
2025-04-01 15:25:36 +0800

    chore: watch event logs
    Signed-off-by: BTBMan <1498118157@qq.com>

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { parseAbi } from 'viem';
+import { client } from './viem.config';
+
+const USDT_ADDRESS = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
+const ABI = parseAbi([
+  'event Transfer(address indexed from, address indexed to, uint value)',
+]);
+
+const getEvents = async () => {
+  const block = await client.getBlockNumber();
+
+  client.watchContractEvent({
+    address: USDT_ADDRESS,
+    abi: ABI,
+    eventName: 'Transfer',
+    fromBlock: block,
+    batch: false,
+    onLogs: (logs) => {
+      logs.forEach((log) => {
+        const { args } = log;
+        console.log(`${args.from} -> ${args.to}: ${args.value}`);
+      });
+    },
+  });
+};
+
+const main = async () => {
+  await getEvents();
+};
+
+main();
